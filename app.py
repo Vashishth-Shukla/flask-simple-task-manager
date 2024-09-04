cbe0cffcfa738bb5fd79a5bebb1be008c0d2f008
Vashishth-Shukla <vashishth.shukla1@gmail.com>
2024-09-04 17:30:41 +0200

    creats a Flast Task Manager

---
+++ app.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+# /// is relative path and //// is absolute path
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+# Disable the overhead warning for modification tracking
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Initialize database
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return f"<Task {self.id}>"
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    if request.method == "POST":
+        task_content = request.form["content"]
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Task was not added!"
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template("index.html", tasks=tasks)
+
+
+@app.route("/delete/<int:id>")
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "There was a problem deleting the task!"
+
+
+@app.route("/update/<int:id>", methods=["GET", "POST"])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+    if request.method == "POST":
+        task_to_update.content = request.form["content"]
+        try:
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "Could not update the task!"
+    else:
+        return render_template("update.html", task=task_to_update)
+
+
+if __name__ == "__main__":
+    with app.app_context():  # Ensure the app context is pushed
+        db.create_all()  # Create tables within the app context
+    app.run(debug=True)
